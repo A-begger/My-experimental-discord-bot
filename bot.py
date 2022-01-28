797406669f5e5c6071343c2f3df2207ca7a29700
A-begger <abegger8gmail.com>
2022-01-28 18:07:19 +0800

    Fixed .env and commented code

--- bot.py
+++ bot.py
@@ -1,25 +1,30 @@
-
 # bot.py
 import os
 import random
 import discord
 from mcstatus import MinecraftServer
+
 # 1
 from discord.ext import commands
-my_secret = os.environ['TOKEN']
 
-TOKEN = my_secret
-#TOKEN = os.getenv('my_secret')
+from dotenv import load_dotenv
+
+load_dotenv()
+# imports os, discord and .env
+TOKEN = os.getenv('auth')
+# fetches the token from the .env
 
 # 2
-bot = commands.Bot(command_prefix='s!')
-
+bot = commands.Bot(command_prefix='-')
+# bot prefix
 @bot.event
 async def on_ready():
+    # this is executed when the bot is ready to run
     print(f'{bot.user.name} has connected to Discord!')
 @bot.command(name='status')
+# is a command, does this if status
 async def server_lookup(ctx):
-    server = MinecraftServer.lookup("TheBlackHatClan.aternos.me")
+    server = MinecraftServer.lookup("inthessmokes.aternos.me")
     # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
     status = server.status()
     if status.players.online == 0:
@@ -29,5 +34,10 @@ async def server_lookup(ctx):
         online = (f"The server is online and has",{status.players.online},"players online")
         response = online
     await ctx.send(response)
+@bot.command(name='ping') #ping pong
+async def pong(ctx):
+  response = "pong!"
+  await ctx.send(response)
 
 bot.run(TOKEN)
+# runs the bot with the token
